bcacfeb65105b7465ec63556d7e425e11e868e8c
bforis <faradianboris@outlook.com>
2020-09-07 18:59:22 +0200

    serializers, views + models create data for students - back end finished

--- RESUME/RestAPP/models.py
+++ RESUME/RestAPP/models.py
@@ -8,5 +8,8 @@ class Student(models.Model):
     course = models.CharField(max_length=100)
     rating = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         ordering = ['name']
--- RESUME/RestAPP/views.py
+++ RESUME/RestAPP/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-
+# from django.shortcuts import render
+# from django.http import JsonResponse
+from .models import Student
+from rest_framework.viewsets import ModelViewSet
+from .serializers import StudentSerializer
 # Create your views here.
 
 
-def index(request):
+class StudentsViewSet(ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+"""def index(request):
+    # model to dict
+    # list(Student.objects.values())
+
+    # students = Student.objects.all()
     students = []
+
+    for student in Student.objects.all():
+        students.append({
+            'name': student.name,
+            'course': student.course,
+            'rating': student.rating
+        })
+
     return JsonResponse(students, safe=False)
+"""
